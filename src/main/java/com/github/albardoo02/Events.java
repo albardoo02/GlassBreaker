b95b80230c6f3f0bfb26f5f169074e2279b94ff8
albardoo02 <albardoo02@gmail.com>
2022-08-01 15:06:55 +0900

    新機能追加

--- src/main/java/com/github/albardoo02/Events.java
+++ src/main/java/com/github/albardoo02/Events.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -26,6 +27,42 @@ public class Events implements Listener {
                         block.setType(Material.AIR);
                         player.getInventory().addItem(new ItemStack(Material.GLASS));
                         player.playSound(player.getLocation(), Sound.BLOCK_GLASS_BREAK, 1.0f, 1.0f);
+                    }
+                    if (block.getType() == Material.CARROTS) {
+                        event.setCancelled(true);
+                    }
+                    if (block.getType() == Material.POTATOES) {
+                        event.setCancelled(true);
+                    }
+                    if (block.getType() == Material.WHEAT) {
+                        event.setCancelled(true);
+                    }
+                    if (block.getType() == Material.PUMPKIN_STEM) {
+                        event.setCancelled(true);
+                    }
+                    if (block.getType() == Material.ATTACHED_PUMPKIN_STEM) {
+                        event.setCancelled(true);
+                    }
+                    if (block.getType() == Material.BEETROOTS) {
+                        event.setCancelled(true);
+                    }
+                    if (block.getType() == Material.NETHER_WART) {
+                        event.setCancelled(true);
+                    }
+                }
+            }
+        }
+        if(action.equals(Action.RIGHT_CLICK_BLOCK)) {
+            if (event.getItem() != null) {
+                if (event.getItem().getItemMeta().equals((ItemManager.hoe.getItemMeta()))) {
+                    if (block.getType() == Material.GRASS_BLOCK) {
+                        event.setCancelled(true);
+                    }
+                    if (block.getType() == Material.DIRT) {
+                        event.setCancelled(true);
+                    }
+                    if (block.getType() == Material.COARSE_DIRT) {
+                        event.setCancelled(true);
                     }
                 }
             }
